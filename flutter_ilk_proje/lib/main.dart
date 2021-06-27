ab2257d051b2b741917e1e1c72f0daee3be8d63e
islamoktay <43931758+islamoktay@users.noreply.github.com>
2021-06-28 01:04:34 +0300

    studied more

--- flutter_ilk_proje/lib/main.dart
+++ flutter_ilk_proje/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ilk_proje/image_widgets.dart';
-import 'package:flutter_ilk_proje/my_counter_page.dart';
+// import 'package:flutter_ilk_proje/image_widgets.dart';
+// import 'package:flutter_ilk_proje/my_counter_page.dart';
 import 'package:flutter_ilk_proje/temel_buton_turleri.dart';
 
 void main() {
