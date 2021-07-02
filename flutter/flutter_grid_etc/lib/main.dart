ca887be7cc986cdbbd3b06c379dbe5bc23c2aa47
islamoktay <43931758+islamoktay@users.noreply.github.com>
2021-07-02 09:42:19 +0300

    studied more

--- flutter/flutter_grid_etc/lib/main.dart
+++ flutter/flutter_grid_etc/lib/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_grid_etc/grid_view_kullanimi.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Dersleri',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Gridview Dersleri',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: GridViewOrnek(),
+      ),
+    );
+  }
+}
