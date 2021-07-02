93413141abf38e9e6f3d4ae080711cc1fc61c3dd
islamoktay <43931758+islamoktay@users.noreply.github.com>
2021-07-02 09:47:49 +0300

    studied more

--- flutter/flutter_grid_etc/lib/grid_view_kullanimi.dart
+++ flutter/flutter_grid_etc/lib/grid_view_kullanimi.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 class GridViewOrnek extends StatelessWidget {
-  const GridViewOrnek({ Key? key }) : super(key: key);
+  const GridViewOrnek({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return GridView.count(
+      crossAxisCount: 3,
+      primary: false,
+      padding: EdgeInsets.all(10),
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 40,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
     );
   }
 }
