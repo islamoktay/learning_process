1f77840998b943f3dd5ea080c45d16d37edff5aa
islamoktay <43931758+islamoktay@users.noreply.github.com>
2021-07-02 10:05:38 +0300

    studied more

--- flutter/flutter_grid_etc/lib/grid_view_kullanimi.dart
+++ flutter/flutter_grid_etc/lib/grid_view_kullanimi.dart
@@ -5,9 +5,158 @@ class GridViewOrnek extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return GridView.builder(
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            alignment: Alignment.center,
+            color: Colors.teal[100 * (index % 9)],
+            child: Text(
+              'Merhaba Flutter',
+              textAlign: TextAlign.center,
+            ),
+          );
+        });
+  }
+
+  GridView gridViewExtentKullanimi() {
+    return GridView.extent(
+      maxCrossAxisExtent: 300,
+      primary: true,
+      padding: EdgeInsets.all(10),
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 40,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.teal.shade100,
+          child: Text(
+            'Merhaba Flutter',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
+    );
+  }
+
+  GridView gridviewCountKullanimi() {
     return GridView.count(
       crossAxisCount: 3,
-      primary: false,
+      reverse: true,
+      primary: true,
       scrollDirection: Axis.horizontal,
       padding: EdgeInsets.all(10),
       crossAxisSpacing: 20,
