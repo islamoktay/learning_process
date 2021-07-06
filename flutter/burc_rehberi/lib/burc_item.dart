39cb8ca3fe0acdccd10c39071e202ac4abb90c1b
islamoktay <43931758+islamoktay@users.noreply.github.com>
2021-07-06 04:59:17 +0300

    studied more

--- flutter/burc_rehberi/lib/burc_item.dart
+++ flutter/burc_rehberi/lib/burc_item.dart
@@ -1,3 +1,4 @@
+import 'package:burc_rehberi/burc_detay.dart';
 import 'package:burc_rehberi/model/burc.dart';
 import 'package:flutter/material.dart';
 
@@ -8,25 +9,38 @@ class BurcItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var myTextStyle = Theme.of(context).textTheme;
-    return Card(
-      elevation: 4,
-      child: ListTile(
-        leading: CircleAvatar(
-          child: Image.asset(
-            'assets/images/' + listelenenBurc.burcKucukResim,
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Card(
+        elevation: 4,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => BurcDetay(secilenBurc: listelenenBurc),
+                ),
+              );
+            },
+            leading: CircleAvatar(
+              child: Image.asset(
+                'assets/images/' + listelenenBurc.burcKucukResim,
+              ),
+            ),
+            title: Text(
+              listelenenBurc.burcAdi,
+              style: myTextStyle.headline5,
+            ),
+            subtitle: Text(
+              listelenenBurc.burcTarihi,
+              style: myTextStyle.subtitle1,
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.pink,
+            ),
           ),
-        ),
-        title: Text(
-          listelenenBurc.burcAdi,
-          style: myTextStyle.headline5,
-        ),
-        subtitle: Text(
-          listelenenBurc.burcTarihi,
-          style: myTextStyle.subtitle1,
-        ),
-        trailing: Icon(
-          Icons.arrow_forward_ios,
-          color: Colors.pink,
         ),
       ),
     );
