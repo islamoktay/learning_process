8d7cedb0eb64ff00d050a47aade84e9c05b4d8d5
islamoktay <43931758+islamoktay@users.noreply.github.com>
2021-07-18 19:29:12 +0300

    studied more

---
+++ flutter/flutter_fonts_etc/lib/anasayfa.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnaSayfa extends StatelessWidget {
+  const AnaSayfa({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lime.shade300,
+      child: Center(
+        child: Text("ANASAYFA"),
+      ),
+    );
+  }
+}
---
+++ flutter/flutter_fonts_etc/lib/aramasayfasi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AramaSayfasi extends StatelessWidget {
+  const AramaSayfasi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lime.shade300,
+      child: Center(
+        child: Text("ARAMA SAYFASI"),
+      ),
+    );
+  }
+}
