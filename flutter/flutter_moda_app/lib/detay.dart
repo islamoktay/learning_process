154d8fc80319428e87adac0eef8238db0bed6b0d
islamoktay <43931758+islamoktay@users.noreply.github.com>
2021-08-05 15:57:08 +0300

    studied more

--- flutter/flutter_moda_app/lib/detay.dart
+++ flutter/flutter_moda_app/lib/detay.dart
@@ -12,18 +12,112 @@ class _DetayState extends State<Detay> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Hero(
-        tag: widget.imgPath,
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(widget.imgPath),
-              fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Hero(
+            tag: widget.imgPath,
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(widget.imgPath),
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
           ),
-        ),
+          Positioned(
+            left: 15,
+            right: 15,
+            bottom: 15,
+            child: Material(
+              borderRadius: BorderRadius.circular(10),
+              elevation: 4,
+              child: Container(
+                height: 250,
+                width: MediaQuery.of(context).size.width - 30,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Container(
+                            height: 120,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(color: Colors.grey),
+                              image: DecorationImage(
+                                image: AssetImage(
+                                  "assets/dress.jpg",
+                                ),
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "LAMINATED",
+                              style: TextStyle(
+                                  fontFamily: "Montserrat",
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Louis Vuitton",
+                              style: TextStyle(
+                                fontFamily: "Montserrat",
+                                fontSize: 16,
+                                color: Colors.grey,
+                                // fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Container(
+                              height: 30,
+                              width: MediaQuery.of(context).size.width - 170,
+                              child: Text(
+                                "One button V-neck sling long-sleeved waist female stitching dress",
+                                style: TextStyle(
+                                  fontFamily: "Montserrat",
+                                  fontSize: 13,
+                                  color: Colors.grey,
+                                  // fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Divider(
+                      indent: 5,
+                      endIndent: 5,
+                    ),
+                    Row(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
